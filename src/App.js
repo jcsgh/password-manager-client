3f265ea1edcc93706aa6ff2a2a23dcfb61577070
jcsgh <business.joshuac@gmail.com>
2022-03-16 15:40:12 -0500

    basic webpage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import './App.css';
-import Nav from './components/Nav';
+
+import Header from './components/Header';
+import NavBar from './components/NavBar';
+import New from './components/New';
+import Passwords from './components/Passwords';
+import About from './components/About';
 import Footer from './components/Footer';
+import Home from './components/Home';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { useEffect } from 'react'
+import {
+  Route,
+  Switch,
+  Link
+} from "react-router-dom";
 
 // import ToDoList from './ToDoList'
 // import ToDoForm from './ToDoForm'
@@ -20,10 +33,24 @@ function App() {
   // }, [])
 
   return (
-    <div className="App">
-      <h1>Password Manager</h1>
-      <Nav />
+    <div className="page-container">
+      <div className="content-wrap">
+      <Header/>
+      <NavBar/>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/passwords">
+            <Passwords />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </div>
       <Footer />
+
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+      <div>
+        <h1>Password Manager</h1>
+      </div>
+    );
+  }
+  
+  export default Header;
